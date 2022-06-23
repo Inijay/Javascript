7746c093e53398e394b05790170a6cec1e2d5533
Inioluwa Jacob <51286535+Inijay@users.noreply.github.com>
2022-06-23 20:56:35 +0100

    new

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// declaring variables
 const fullname = "Inioluwa Jacob"
 const height = "6'0"
 const country = "Nigeria"
 
+// printing the fullname, height and country
+console.log(fullname + " " + height + " " + country)
